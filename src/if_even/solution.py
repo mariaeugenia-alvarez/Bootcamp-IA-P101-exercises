0a336ccd23748c02dd4005caf352924de29fa6fb
Maru <mariaeu.alvarez@gmail.com>
2025-06-06 12:53:47 +0200

    Add solution to if even

---
+++ src/if_even/solution.py
@@ -0,0 +1,7 @@
+def solution(a):
+    return  a % 2 == 0
+    
+
+if __name__ == '__main__': 
+    b=int(input("mete un numero: "))
+    print(solution(b))
---
+++ src/if_even/test_solution.py
@@ -0,0 +1,5 @@
+from src.if_even.solution import solution
+
+def test_solution():
+    #Test case 1
+    assert solution(2) == True
