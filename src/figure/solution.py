6b262eccdcf74f6b0e6f7623bf56b180d9b40f81
Maru <mariaeu.alvarez@gmail.com>
2025-06-06 16:27:18 +0200

    Add solution and tests for identifying polygon names based on number of sides

---
+++ src/figure/solution.py
@@ -0,0 +1,24 @@
+def solution(entry):
+
+    if entry ==3:
+        result = "triangulo"
+    elif entry == 4:
+        result = "cuadrado"
+    elif entry == 5:
+        result = "pentagono"
+    elif entry == 6:
+        result = "hexagono"
+    elif entry == 7:
+        result = "heptagono"
+    elif entry == 8:
+        result = "octogono"
+    elif entry == 9:
+        result = "eneagono"
+    elif entry == 10:
+        result = "decagono"
+    else:
+        result = "error: número fuera de rango"
+    return  result
+    
+
+
---
+++ src/figure/test_solution.py
@@ -0,0 +1,7 @@
+from src.figure.solution import solution
+
+def test_solution():
+    #Test case 
+    assert solution(3) == "triangulo"
+    assert solution(8) == "octagono"
+    assert solution(12) == "error: número fuera de rango"
