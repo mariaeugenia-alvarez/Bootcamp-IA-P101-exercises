a873af0a8cca418b90a176e1718b62c16cfb9e50
Maru <mariaeu.alvarez@gmail.com>
2025-06-16 11:25:46 +0200

    Refactor invoice calculation in zoo module for improved readability and maintainability

--- src/zoo/solution.py
+++ src/zoo/solution.py
@@ -1,32 +1,38 @@
 def get_invoice(age: list):
-        nbaby=0
-        nchildren=0
-        nadult=0
+        
+        tickets= [["Baby",0,0],["Children",14,0],["Adult",23,0]]
         pbaby=0
         pchildren=0
         padult=0
         casebaby="Niños de 2 años o menos"
         casechildren="Niños de 3 a 12 años"
         caseadult="Niños de 13 a 65 años"
-          
-        for i in age:
-                if i<=2:
-                   nbaby+=1
-                   pbaby=nbaby*0
-                elif 3<=i<=12:
-                   nchildren+=1
-                   pchildren+=nchildren*14
-                elif 13<=i<65:
-                   nadult+=1
-                   padult+=nadult*23             
                    
-        total=pbaby+pchildren+padult
-        txttotal = "El precio total es: {tot}. ".format(tot=total)
-        txtbaby = "{txt}: {ntickets} = {price}. ".format(ntickets=nbaby,price=pbaby,txt=casebaby)
-        txtchildren = "{txt}: {ntickets} = {price}. ".format(ntickets=nchildren,price=pchildren,txt=casechildren)
-        txtadult = "{txt}: {ntickets} = {price}. ".format(ntickets=nadult,price=padult,txt=caseadult)
+        pbaby, pchildren, padult, total = get_price(tickets,age)
+        return format_output(tickets[0][2], tickets[1][2], tickets[2][2], pbaby, pchildren, padult, total, casebaby, casechildren, caseadult)
 
-        return txttotal + txtbaby + txtchildren + txtadult
+def get_price(tickets,age):
+   for i in age:  
+      if i<=2:
+         tickets[0][2]+=1
+      elif 3<=i<=12:
+         tickets[1][2]+=1
+      elif 13<=i<65:
+         tickets[2][2]+=1   
+
+   pbaby = tickets[0][2] * tickets[0][1]
+   pchildren = tickets[1][2] * tickets[1][1]
+   padult = tickets[2][2] * tickets[2][1]
+   total=pbaby+pchildren+padult
+   return pbaby,pchildren,padult,total
+
+def format_output(nbaby, nchildren, nadult, pbaby, pchildren, padult, total, casebaby, casechildren, caseadult):
+    txttotal = "El precio total es: {tot}. ".format(tot=total)
+    txtbaby = "{txt}: {ntickets} = {price}. ".format(ntickets=nbaby,price=pbaby,txt=casebaby)
+    txtchildren = "{txt}: {ntickets} = {price}. ".format(ntickets=nchildren,price=pchildren,txt=casechildren)
+    txtadult = "{txt}: {ntickets} = {price}. ".format(ntickets=nadult,price=padult,txt=caseadult)
+    
+    return txttotal + txtbaby + txtchildren + txtadult
 
 def is_finish_input(next_input):
     return next_input == ""
