ac2783422537585253c235179a4cb6c54e71f928
Maru <mariaeu.alvarez@gmail.com>
2025-06-16 13:32:55 +0200

    Add number conversion and global dictionary to Caesar cipher implementation

--- src/cesar_encode/solution.py
+++ src/cesar_encode/solution.py
@@ -1,4 +1,24 @@
+global_dictionary = list("abcdefghijklmnñopqrstuvwxyz")
+
+def to_numbers(literal_list, dictionary):
+   number_list=[]
+   for word in literal_list:
+      if word in dictionary:
+         number = dictionary.index(word)
+      else:
+         number = word
+      number_list.append(number)
+   return number_list
+
+def to_string(numbers_list):
+   return []
+
+
 def transpose(literal,step):
-      dictionary = list("abcdefghijklmnñopqrstuvwxyz")
+      
       literal_list=list(literal)
+      number_list = to_numbers(literal_list, dictionary=global_dictionary)
+      #cifrar
+      string_list = to_string(number_list)
+
       return ("bki bci")
--- src/cesar_encode/test_solution.py
+++ src/cesar_encode/test_solution.py
@@ -1,6 +1,8 @@
-from src.cesar_encode.solution import transpose
+from src.cesar_encode.solution import transpose, to_numbers, global_dictionary
 
 def test_solution():
     #Test case
     assert transpose("zig zag",2) == "bki bci"
+    assert to_numbers ("zig", global_dictionary)==[26,8,6]
+
     
