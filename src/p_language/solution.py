b8ff84b641a9a74e7da8eb220c34a59b9f215f3d
Maru <mariaeu.alvarez@gmail.com>
2025-06-18 16:05:42 +0200

    Update character conversion functions to accept consonant parameter and adjust test cases accordingly

--- src/p_language/solution.py
+++ src/p_language/solution.py
@@ -1,15 +1,15 @@
-def convert_char(char):
+def convert_char(char,consonant):
     new_char = char
     vowel = "aeiou"
     for i in range(len(vowel)):
       if char == vowel[i]:
-         new_char = vowel[i] + "p" + vowel[i]
+         new_char = vowel[i] + consonant + vowel[i]
          break
     return new_char
 
 
-def p_sentence(sentence):
+def p_sentence(sentence,consonant):
     new_sentence=""
     for letter in sentence:    
-        new_sentence += convert_char(letter)
+        new_sentence += convert_char(letter,consonant)
     return new_sentence
--- src/p_language/test_solution.py
+++ src/p_language/test_solution.py
@@ -1,9 +1,9 @@
 from src.p_language.solution import convert_char, p_sentence
 def test_solution():
     #Test case
-    assert convert_char("a") == "apa"
-    assert convert_char("i") == "ipi"
-    assert convert_char("B") == "B"
-    assert p_sentence("Basico") == "Bapasipicopo"
-    assert p_sentence("Raro") == "Raparopo"
-    assert p_sentence("Hola mundo") == "Hopolapa mupundopo"
+    assert convert_char("a", "p") == "apa"
+    assert convert_char("i","p") == "ipi"
+    assert convert_char("B", "p") == "B"
+    assert p_sentence("Basico","z") == "Bazasizicozo"
+    assert p_sentence("Raro","t") == "Rataroto"
+    assert p_sentence("Hola mundo", "p") == "Hopolapa mupundopo"
