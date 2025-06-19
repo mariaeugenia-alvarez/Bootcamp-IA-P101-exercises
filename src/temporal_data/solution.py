f36188c4746acf40fcc670a24a25dbc76e2ee622
Maru <mariaeu.alvarez@gmail.com>
2025-06-19 18:54:49 +0200

    Refactor temporal data processing functions to improve clarity and add handling for increasing number sequences

--- src/temporal_data/solution.py
+++ src/temporal_data/solution.py
@@ -1,31 +1,39 @@
-#[72,72,75,75,70,74,74,76]
-
 def index_difference(i,current_number,next_number):
-    
+    index = i+1
     id_dictionary={}
-    id_dictionary[i]= next_number - current_number
+    id_dictionary[index]= next_number - current_number
 
     return id_dictionary
 
-
-
-
-
-def numbers_incremental_series(secuence:list):
-    current_number = 0
-    next_number = 0
-    lgth= len(secuence)
-
-    for i in range(lgth):
+def changes_in_list(secuence, lgth, id_acumm_dictionary):
+    for i in range(lgth-1):
         current_number=secuence[i]
         next_number=secuence[i+1]
 
-        if current_number == next_number:
-            continue
-        else:
-            id__dictionary = index_difference(i,current_number,next_number)
+        if current_number != next_number:
+            id_dictionary = index_difference(i,current_number,next_number) 
+            id_acumm_dictionary.update(id_dictionary)
+
+def filter_increasing_numbers(secuence, id_acumm_dictionary, series_list):
+    for index in id_acumm_dictionary.keys():
+        if id_acumm_dictionary[index]>=0:
+            series_list.append(secuence[index])
+
+def numbers_incremental_series(secuence:list):
+    lgth= len(secuence)
+    id_acumm_dictionary ={}
+    series_list=[]
+
+    if lgth == 0:
+        return series_list
+
+    changes_in_list(secuence, lgth, id_acumm_dictionary)
+
+    filter_increasing_numbers(secuence, id_acumm_dictionary, series_list)
+
+    return series_list
 
 
 
-        return [75,74,76]
+
 
--- src/temporal_data/test_solution.py
+++ src/temporal_data/test_solution.py
@@ -1,8 +1,8 @@
 from src.temporal_data.solution import numbers_incremental_series,  index_difference
 def test_solution():
     #Test case
-    assert index_difference(1,72,75) == {1:3}
+    assert index_difference(1,72,75) == {2:3}
+    assert numbers_incremental_series([72,72,75]) == [75]
     assert numbers_incremental_series([72,72,75,75,70,74,74,76]) == [75,74,76]
-
 
    
